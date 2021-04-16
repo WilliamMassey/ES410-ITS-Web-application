7276101c0961b871f185ecb8926971f24a5eeb87
Will Massey <w.massey.1@warwick.ac.uk>
2021-04-16 19:01:38 +0100

    remove legacy code and update car-create to create mappings if the car already exists

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,18 +5,12 @@ from rest_framework.authtoken.views import obtain_auth_token
 # This connects the /accounts/ urls to the view functions
 # N.B. the format <str:Parameter_Name> means that a parameter is passed in the url 
 urlpatterns = [
-    path('register', views.register, name='register'),
-    path('login', views.login, name='login'),
-    path('logout', views.logout, name='logout'),
-    path('add_booking', views.add_booking, name='add_booking'),
-    path('add_car', views.add_car, name ='add_car'),
-
     path('car-api', views.car_api, name ='car-api'),
-    path('car-view/', views.car_view, name ='car-view'),
+    path('car-view/', views.car_view2, name ='car-view'),
     path('car-detail/<str:car_number_plate>', views.car_detail2, name ='car-detail'), 
-    path('car-create/', views.car_create, name ='car-create'),
+    path('car-create/', views.car_create2, name ='car-create'),
     path('car-update/<str:car_number_plate>', views.car_update2, name ='car-update'),
-    path('car-delete/<str:car_number_plate>', views.car_delete, name ='car-delete'),
+    path('car-delete/<str:car_number_plate>', views.car_delete2, name ='car-delete'),
 
     path('booking_api', views.booking_api, name ='booking_api'),
     path('booking-view/', views.booking_view, name ='booking-view'),
--- accounts/views.py
+++ accounts/views.py
@@ -75,7 +75,7 @@ def car_api(request):
 @authentication_classes([TokenAuthentication])
 @permission_classes([IsAuthenticated])
 def car_view2(request):
-    print("car_view called")
+    print(request)
     user  = request.user
     numberplateQS = User_Car_Mapping.objects.filter(user__exact = request.user)  
     numberplates = numberplateQS.values_list('car', flat = True) 
@@ -139,13 +139,24 @@ def car_detail(request, car_number_plate):
 @permission_classes([IsAuthenticated])
 def car_create2(request):
     serializer = CarSerializer(data = request.data) 
-    if serializer.is_valid(): 
+    if serializer.is_valid():
         car = serializer.create(serializer.validated_data) 
         mapping = User_Car_Mapping(car = car, user = request.user) 
-        return Response(serializer.data) 
+        mapping.save() 
+        return Response(serializer.data)
+    elif "car with this car number plate already exists." in serializer.errors['car_number_plate'] and len(serializer.errors['car_number_plate'])==1:
+        car = Car.objects.get(car_number_plate = serializer.data['car_number_plate'])
+        (is_mapped, result) = is_car_user_mapped(request.user, car.car_number_plate)
+        print(is_mapped)
+        print(result)
+        if is_mapped:
+            return Response("The car is already mapped")
+        else: 
+            user_car_mapping = User_Car_Mapping.objects.create(car = car, user = request.user)
+            return Response(f"the car with numberplate {car.car_number_plate} already exists, the user {request.user} is now mapped to it")
     else: 
-        return Response(serializer.errors) 
-    
+        return Response(serializer.errors)
+
 
 @api_view(['POST'])
 def car_create(request):
@@ -158,8 +169,14 @@ def car_create(request):
             return Response(serializer.data) # return the serialized object
         elif "car with this car number plate already exists." in serializer.errors['car_number_plate'] and len(serializer.errors['car_number_plate'])==1:
             car = Car.objects.get(car_number_plate = serializer.data['car_number_plate'])
-            user_car_mapping = User_Car_mapping.objects.create(car = car, user = request.user)
-            return Response("CAr already existed, so a mapping was added")
+            (is_mapped, result) = is_car_user_mapped(request.user, car.car_number_plate)
+            print(is_mapped)
+            print(result)
+            if is_mapped:
+                return Response("The car is already mapped")
+            else: 
+                user_car_mapping = User_Car_Mapping.objects.create(car = car, user = request.user)
+                return Response(f"the car with numberplate {car.car_number_plate} already exists, the user {request.user} is now mapped to it")
         else: 
             return Response(serializer.errors) # return the error response, outlining, what was wrong with the serialized data 
     else:
@@ -378,279 +395,3 @@ def booking_delete(request, pk):
         return Response("booking id " + str(pk) + " has been successfully deleted") # return message stating that it has been successfully deleted
     else: 
         return Response("ERROR: YOU ARE NOT LOGGED IN")
-
-
-##### Rendering Views #####
-# Notes:
-# 1) These are currently redundant views so as a result have bugs
-# 2) These use default django workflow where standard JavaScript, CSS and HTML are used, for the final implementation react will be used.
-# 3) Although rendering views are required, the functionality has changed so drastically, that none of these will be used by kept as a reference  
-
-# Render the login page
-def accounts(request):
-
-    #by default login 
-    return render(request, 'login.html')
-
-# redirect to login page, recive loggin form, check whether the user data is valid, and if so log in and return to home page, if not return to login page with an error message
-def login(request):
-    ### ADD EMAIL CONFIRMED ###
-    if request.method == 'POST':
-        uni_id = request.POST['uni_id']
-        password = request.POST['password']
-        
-        user = auth.authenticate(username = uni_id, password = password)
-        if user is not None:
-            auth.login(request, user)
-            return redirect('/')
-        else:
-            messages.info(request, 'The details entered do not exist.')
-            return redirect('login')
-
-    else:
-        return render(request, 'login.html')
-
-# redirect to register page recive register form, check inputs against criteria and if valid create the user and redirect to home page, if not return to the register page and display error messages 
-def register(request):
-    if request.method == 'POST':
-        first_name = request.POST['first_name']
-        last_name = request.POST['last_name']
-        uni_id = request.POST['uni_id']
-        password = request.POST['password']
-        password_confirm = request.POST['password_confirm']
-        email = request.POST['email']
-        email_confirm = request.POST['email_confirm']
-
-        ###### CONDITOINS ######
-        ### NAMES ###
-        name_valid = True
-        # are the names blank
-        if (not first_name) or (not last_name):
-            messages.info(request, "The names can't be left blank.", extra_tags='names')
-            name_valid = False
-        ### only spaces ###
-
-        ### UNI-ID ###
-        uni_id_valid = True
-        # uni_id length
-        if len(uni_id) != 8:
-            messages.info(request, 'University ID is 8 charaters long.', extra_tags='uni_id')
-            uni_id_valid = False
-            
-        # uni id take
-        if User.objects.filter(username = uni_id).exists():
-            messages.info(request, 'This university Id is already in use.', extra_tags='uni_id')
-            uni_id_valid = False
-        ### add valid uni ID ###
-
-        ### PASSWORDS ###
-        password_valid = True
-        # passwords match
-        if password!= password_confirm:
-            messages.info(request,"The passwords don't match.", extra_tags='password')
-            password_valid = False
-
-        # password not too short
-        if len(password) <7:
-            messages.info(request, "The password needs to be at least 7 charaters long.", extra_tags='password')
-            password_valid = False
-        ### passwords contain cirtain charaters ###
-
-        ### EMAILS ###
-        email_valid = True
-        # emails match
-        if email != email_confirm:
-            messages.info(request, "The emails given don't match.", extra_tags='email')
-            email_valid = False
-            
-        # email not taken
-        if User.objects.filter(email = email).exists():
-            messages.info(request, 'This email has already been taken.', extra_tags='email')
-            email_valid = False
-
-        ###### CHECK CONDITIONS ######
-        if name_valid and uni_id_valid and password_valid and email_valid:
-            user = User.objects.create_user(username=uni_id, password=password, email=email,first_name=first_name,last_name=last_name)
-            user.save()
-            print("user created")
-            return redirect("/")
-        else:
-            return redirect('register')
-    #by default login 
-    else:
-        return render(request, 'register.html')
-
-# redirect to add_car page, recive car form, check conditions and determine whether the inputs are valid, if they are create a car and a UCM, if not return to the add car page with error messages
-def add_car(request):
-    if request.user.is_authenticated == True:
-        if request.method == 'POST':
-            car_number_plate =  request.POST['car_number_plate']
-            car_number_plate_confirmation = request.POST['car_number_plate_confirmation']
-            manufacturer = request.POST['manufacturer']
-            car_colour = request.POST['car_colour']
-
-            car_number_plate_is_valid = True
-            car_colour_is_valid = True
-            manufacturer_is_valid = True
-            ###### CONDITIONS ######
-
-            ### NUMBER PLATE ###
-            if len(car_number_plate) > 8:
-                car_number_plate_is_valid = False
-                messages.info(request, 'Car number plate is too long.', extra_tags = 'car_number_plate')
-
-            if len(car_number_plate) == 0:
-                car_number_plate_is_valid = False
-                messages.info(request, "Don't leave the car number plate field blank.", extra_tags = 'car_number_plate')
-
-            if Car.objects.filter(car_number_plate = car_number_plate).exists():
-                messages.info(request,'This car has already been registered', extra_tags = 'car_number_plate')
-                car_number_plate_is_valid = False
-
-            if car_number_plate != car_number_plate_confirmation:
-                messages.info(request, "The number plates added don't match")
-                car_number_plate_is_valid = False
-            
-
-            ### MANUFACTURER ###
-            if len(manufacturer) == 0:
-                car_number_plate_is_valid = False
-                messages.info(request, "Don't leave the manufacturer field blank.", extra_tags = 'manufacturer')
-
-
-            ### CAR COLOUR ###
-            if car_colour == 'x':
-                car_colour_is_valid = False
-                messages.info(request, 'Please select a car colour.', extra_tags='car_colour')                
-
-            car_number_plate = car_number_plate.upper()
-            ###### CHECK CONDITIONS ######
-            if car_number_plate_is_valid and manufacturer_is_valid and car_colour_is_valid:
-                # CREATE car and user_car_mapping objects
-                new_car = Car(car_number_plate=car_number_plate, colour = car_colour, manufacturer = manufacturer)
-                
-                mapping = User_Car_Mapping(car = new_car, user = request.user)
-
-                new_car.save()
-                mapping.save()
-                print("all fields valid, car and mapping added")
-                return redirect('/')
-            else:
-                print("some fields are in valid redirect to add_car")
-                return redirect('add_car')
-        else:
-            print("first visit to add car rendering page")
-            return render(request, 'add_car.html')
-    else: 
-        print("not logged in go to login page")
-        return render(request, 'login.html')
-
-# log the current user out
-def logout(request):
-    auth.logout(request)
-    return redirect('/')
-
-# redirect to the add_booking page, recive the form data and check whether the data is valid, if not return to the add booking page, if it is add the booking, and redirect to home page.
-def add_booking(request):
-    if request.user.is_authenticated == True:
-        if request.method == 'POST':
-            user = request.user
-            car_number_plate =  request.POST['car']
-            start_datetime_str = request.POST['start_datetime']
-            end_datetime_str = request.POST['end_datetime']
-            carpark = request.POST['carpark']
-
-            # setting criteria as true
-            car_is_valid = True
-            start_datetime_is_valid = True
-            end_datetime_is_valid = True
-            carpark_is_valid = True
-            
-            ###### REFROMAT DATETIMES ######
-            # maximum difference between start and end
-            max_diff = timedelta(days=2)
-            
-            start_datetime = conv_html_datetime(start_datetime_str)
-            end_datetime = conv_html_datetime(end_datetime_str)
-            
-            ###### Conditions ######
-            ### ADD BOOKING COLISSION CHECK ###
-            ### START DATE ###
-            # field is empty
-            if not start_datetime:
-                start_datetime_is_valid = False
-                messages.info(request, "Don't leave the start date or time blank.", extra_tags='start_datetime')
-
-            # before current date
-            if datetime.today().date() > start_datetime.date():
-                start_datetime_is_valid = False
-                messages.info(request, 'The date selected is before today.', extra_tags='start_datetime')
-
-            # today but before current time 
-            if datetime.now().time() > start_datetime.time() and datetime.today().date() == start_datetime.date():
-                start_datetime_is_valid = False
-                messages.info(request, 'The time selected is before now.', extra_tags='start_datetime')
-
-            # date is 1/1/2000 
-            if start_datetime == datetime(2000,1,1):
-                messages.info(request, 'A fault has occoured or you have entered the date 1/1/2000', extra_tags='start_datetime')
-                start_datetime_is_valid = False
-
-            ### END DATE ###
-            # field is empty
-            if not end_datetime:
-                end_datetime_is_valid = False
-                messages.info(request, "Don't leave the end date or time blank.", extra_tags='end_datetime')
-
-            # today but before current time 
-            if datetime.now().time() > end_datetime.time() and datetime.today().date() == end_datetime.date():
-                end_datetime_is_valid = False
-                messages.info(request, 'The time selected is before now.', extra_tags='end_datetime')
-
-            # before current date
-            if datetime.today().date() > end_datetime.date():
-                end_datetime_is_valid = False
-                messages.info(request, 'The date or time selected is before today.', extra_tags='end_datetime')
-
-            # start and end are far apart
-            if (end_datetime - start_datetime) > max_diff:
-                end_datetime_is_valid = False
-                messages.info(request, 'The length of the booking is too long.')
-
-            # date is 1/1/2000 
-            if end_datetime == datetime(2000,1,1):
-                messages.info(request, 'A fault has occoured or you have entered the date 1/1/2000', extra_tags='end_datetime')
-                end_datetime_is_valid = False
-
-
-            ### CARPARK ###
-            # field is empty
-            if carpark == 'x':
-                carpark_is_valid = False
-                messages.info(request, "Don't leave the carpark field blank.", extra_tags='carpark')
-                
-            ### CAR ###
-            # field is empty
-            if car_number_plate == 'x':
-                car_is_valid = False
-                messages.info(request, "Don't leave the car field blank.", extra_tags='car')
-
-            ###### CHECK CONDITIONS ######
-            if car_is_valid and carpark_is_valid and end_datetime_is_valid and start_datetime_is_valid:
-                print("booking fields entered correctly sending to home")
-                car = Car.objects.get(car_number_plate__exact = car_number_plate)
-                carpark_obj = Carpark.objects.get(name__exact = carpark)
-                Booking.objects.create(user = user, car = car, start_datetime = start_datetime, end_datetime = end_datetime, carpark = carpark_obj)
-                return redirect('/')
-                
-            else:
-                print("booking fields entered incorrectly returning to booking")
-                return redirect('add_booking')
-        else:
-            cars = list(User_Car_Mapping.objects.filter(user = request.user).values_list('car', flat=True))
-            #print("From add_Bookings page "+str(cars))
-            carparks = Carpark.objects.all()
-            print("Carparks " + str(carparks))
-            return render(request, 'add_booking.html', {'cars' : cars, 'carparks': carparks})
-    else:
-        redirect('/')
