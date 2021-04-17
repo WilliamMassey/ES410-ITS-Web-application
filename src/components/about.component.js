88c85ea2e00004ec226f66ad7b429378c0d71acc
Rosler Mgbokwere <roslermgb@gmail.com>
2021-04-17 11:23:46 +0100

    fetching car data from API

--- src/components/about.component.js
+++ src/components/about.component.js
@@ -8,31 +8,36 @@ function About(props) {
             console.log(props.token)
     }
 
-    const [cars, setCars] = useState({cars:[]})
+    const [cars, setCars] = useState([])
 
     const loadCars = () =>{
         fetch('http://localhost:8000/accounts/car-view/', {
         method: 'GET',
         headers: {
         'Content-Type': 'application/json',
-        Authorization: `Token ${props.token}`,
-        body:JSON.stringify({})}
+        Authorization: `Token ${props.token}`},
         })
     .then( data => data.json())
     .then(
         data => {
         setCars({cars: data})
+        console.log(props.token)
+        console.log(cars)
+        // console.log(cars.cars[0])
         }
     )
     .catch( error => console.error(error))
     }
     
+    // var carData = cars.cars[0]
     
         return (
             <div className="about-title">
                 <h1>About us</h1>
-                
-                <button onClick={checker}>Load Cars</button>
+                { cars.map( car => {
+                    return <h3 key={car.id}>{car.title}</h3>
+                    })}
+                <button onClick={loadCars}>Load Cars</button>
             </div>
         );
     }
